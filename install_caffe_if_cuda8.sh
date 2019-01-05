de99a551c1e67dd64371c25f44fc38d0a07abde8
Gines Hidalgo <gineshidalgo99@gmail.com>
2019-01-04 19:12:44 -0500

    Simplified sh scripts

--- install_caffe_if_cuda8.sh
+++ install_caffe_if_cuda8.sh
@@ -36,15 +36,6 @@ echo ""
 
 
 
-echo "------------------------- Checking Number of Processors -------------------------"
-NUM_CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
-echo "$NUM_CORES cores"
-exitIfError
-echo "------------------------- Number of Processors Checked -------------------------"
-echo ""
-
-
-
 echo "------------------------- Installing some Caffe Dependencies -------------------------"
 # Basic
 sudo apt-get --assume-yes update
@@ -77,10 +68,10 @@ if [[ $ubuntu_le_14 == true ]]; then
 else
     cp Makefile.config.Ubuntu16_cuda8.example Makefile.config
 fi
-# make all -j$NUM_CORES
-make all -j$NUM_CORES && make distribute -j$NUM_CORES
-# make test -j$NUM_CORES
-# make runtest -j$NUM_CORES
+# make all -j`nproc`
+make all -j`nproc` && make distribute -j`nproc`
+# make test -j`nproc`
+# make runtest -j`nproc`
 exitIfError
 echo "------------------------- Caffe Compiled -------------------------"
 echo ""
